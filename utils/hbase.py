2f2dfd8b1f50333fb41adc4cc058f61facbd6dae
createdbylll <2101776398@qq.com>
2024-12-27 21:24:25 +0800

    'report'

--- utils/hbase.py
+++ utils/hbase.py
@@ -10,31 +10,38 @@ class HbaseConnection:
     
     def close(self):
         self.connection.close()
-    
+
     # ambigous search
     def search_ambigous(self, table_name, columns, query, sort=True):
+        # import jieba
         table = self.get_table(table_name)
         documents = []
         result = {}
+        # import time
+        # t = time.time()
         for key, data in table.scan():
             college = data[b'info:college'].decode('utf-8')
             filename = data[b'info:filename'].decode('utf-8')
             if query in college or query in filename:
                 documents.append(key)
                 result[key] = [filename, college, data[b'data:data']]
+
+        # print('search time:', time.time()-t) 
         if sort:
-            from sklearn.feature_extraction.text import TfidfVectorizer
+            from sklearn.feature_extraction.text import TfidfVectorizer,CountVectorizer
             from sklearn.metrics.pairwise import cosine_similarity
             # for each column, get an similarity score, using tf-idf with scikit-learn
             # finally sort the result by score
             tfidf = TfidfVectorizer()
+            # tfidf = CountVectorizer()
             all_docs = [query]+documents
             tfidf_matrix = tfidf.fit_transform(all_docs)
+            # print('trans time:', time.time()-t)
             similarity_matrix = cosine_similarity(tfidf_matrix)
-            
             sort_result = {}
             for i in range(1, len(similarity_matrix)):
                 sort_result[all_docs[i]] = similarity_matrix[0][i]
             sort_result = dict(sorted(sort_result.items(), key=lambda x: x[1], reverse=True))
+            
             result = [(k, *result[k]) for k in sort_result.keys()]
         return result
